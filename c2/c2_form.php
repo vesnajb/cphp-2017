5824e96906741b0873e1a0509bfe31518f28d90b
Vesna Janakievska-Bugarski <32159101+vesnajb@users.noreply.github.com>
2018-02-09 13:21:15 +0100

    cas 2 dopuna

--- c2/c2_form.php
+++ c2/c2_form.php
@@ -1,6 +1,16 @@
 <?php
 
-// var_dump($_GET);
-print_r($_GET);
+// // var_dump($_GET);
+// print_r($_GET);
+
+// echo $_GET['drzava'];
+// echo '<br>';
+// echo $_GET['grad'];
+
+echo $_POST['drzava'];
+echo '<br>';
+echo $_POST['grad'];
+
+
 
 ?>
